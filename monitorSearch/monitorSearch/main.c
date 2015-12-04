0a7a24a5bf1b8ccb0e8be84a6158b09e0420b7e8
Nabil A <nabilabdu@gatech.edu>
2015-12-04 17:09:37 -0500

    added timing functionality

--- monitorSearch/monitorSearch/main.c
+++ monitorSearch/monitorSearch/main.c
@@ -8,38 +8,73 @@
 
 #include "BellmanFord.h"
 #include "Common.h"
+#include <time.h>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     
-    if (argc != 3) {
-        Warning("main", "Should only have graph's filename and number of edges");
+    if (argc < 5) {
+        Warning("main", "Not enough arguments");
     }
     
     char const *graph_file = argv[1];
-    size_t numberOfEdges = strtol(argv[2], NULL, 10);
-
-    Graph* graph = readGraph(graph_file, numberOfEdges);    
+    size_t sourceID = (size_t) strtol(argv[2], NULL, 10);
+    size_t destID = (size_t) strtol(argv[3], NULL, 10);
+    int nthreads = (int) strtol(argv[4], NULL, 10);
     
-    double mindist = bellmanFord(graph, 4, 3);
-    printf("Bellman-Ford min distance is: %.3f\n", mindist);
+    Graph* graph = readGraph(graph_file);    
     
-    double dijk_min = dijkstra(graph, 4, 3);
-    printf("Dijkstra min distance is: %.3f\n", dijk_min);
-    
-    printf("\n");
-    
-    double bf_dist = bellmanFord(graph, 10, 3);
-    printf("Second Bellman-Ford min distance is: %.2f\n", bf_dist);
+    clock_t begin, end;
     
     
-    double dijk = dijkstra(graph, 10, 3);
-    printf("Second Dijkstra min distance is: %.2f\n", dijk);
+//    double mindist = bellmanFord(graph, sourceID, destID);
+//    printf("BF min distance is: %.3f\n", mindist);
+//    
+//    double dijk_min = dijkstra(graph, sourceID, destID);
+//    printf("Dijkstra min distance is: %.3f\n", dijk_min);
+//    
+//    printf("\n");
+    
+    double avg_times[4] = {0, 0, 0, 0};
+    double num_reps = 10;
+    double bf_dist, dijk, ompbf_dist, omp_dijk_dist;
+    
+    
+    for (int m=0; m < num_reps; m++) {
+        begin = clock();
+        bf_dist = bellmanFord(graph, 10, 3);
+        end = clock();
+        avg_times[0] += (double) (end - begin) / (num_reps * CLOCKS_PER_SEC);
+        
+        
+        begin = clock();
+        dijk = dijkstra(graph, 10, 3);
+        end = clock();
+        avg_times[1] += (double) (end - begin) / (num_reps * CLOCKS_PER_SEC);
+        
+        
+        begin = clock();
+        ompbf_dist = ompBellFord(graph, 10, 3, nthreads);
+        end = clock();
+        avg_times[2] += (double) (end - begin) / (num_reps * CLOCKS_PER_SEC);
+        
+        
+        begin = clock();
+        omp_dijk_dist = ompBellFord(graph, 10, 3, nthreads);
+        end = clock();
+        avg_times[3] += (double) (end - begin) / (num_reps * CLOCKS_PER_SEC);
+    }
+    
+    // Print Results
+    
+    
+    printf("Type\tBF\tDK\tOMPBF\tOMPDK\n");
+    printf("Dist\t%.3f\t%.3f\t%.3f\t%.3f\n", bf_dist, dijk, ompbf_dist, omp_dijk_dist);
+    printf("Time\t%.5f\t%.5f\t%.5f\t%.5f\n", avg_times[0], avg_times[1], avg_times[2], avg_times[3]);
+    
     
     
     freeGraph(graph);
-    
-    printf("Hello, World!\n");
     return 0;
 }
 
