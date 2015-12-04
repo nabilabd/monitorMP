6e42ec32d2603dbe17bccff23c273bc8d3f6a31a
Nabil A <nabilabdu@gatech.edu>
2015-12-04 05:05:21 -0500

    included basic testing for algorithms

--- monitorSearch/monitorSearch/main.c
+++ monitorSearch/monitorSearch/main.c
@@ -12,33 +12,29 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     
-    if (argc < 2) {
-        Warning("main", "missing argument: filename of graph");
-    } else if (argc > 2) {
-        Warning("main", "too many arguments; only using the second one");
+    if (argc != 3) {
+        Warning("main", "Should only have graph's filename and number of edges");
     }
     
     char const *graph_file = argv[1];
     size_t numberOfEdges = strtol(argv[2], NULL, 10);
 
-    Graph* graph = readGraph(graph_file);
-    size_t graphSize = getNumNodes(graph);
-    print_graph(graph);
+    Graph* graph = readGraph(graph_file, numberOfEdges);    
+    
+    double mindist = bellmanFord(graph, 4, 3);
+    printf("Bellman-Ford min distance is: %.3f\n", mindist);
+    
+    double dijk_min = dijkstra(graph, 4, 3);
+    printf("Dijkstra min distance is: %.3f\n", dijk_min);
+    
+    printf("\n");
+    
+    double bf_dist = bellmanFord(graph, 10, 3);
+    printf("Second Bellman-Ford min distance is: %.2f\n", bf_dist);
     
     
-    MetaNode** myArray = make_array( graphSize );
-    InitializeSingleSource(myArray, graphSize, 5);
-    
-    
-    double mindist = bellmanFord(graph, 5, 3, graphSize, myArray);
-    printf("Bellman-Ford min distance is: %f\n\n", mindist);
-    
-    MetaNode** myArray2 = make_array( graphSize );
-    InitializeSingleSource(myArray2, graphSize, 4);
-    
-    double dijk_min = dijkstra(graph, 4, 3, myArray2);
-    printf("Dijkstra min distance is: %f\n", dijk_min);
-    
+    double dijk = dijkstra(graph, 10, 3);
+    printf("Second Dijkstra min distance is: %.2f\n", dijk);
     
     
     freeGraph(graph);
